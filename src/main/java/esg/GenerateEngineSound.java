e2ba61e2f7ae44d2b1126511587ef3cd15318d73
lukak85 <luka.kuzman85@gmail.com>
2021-11-03 23:08:58 +0100

    Initial commit

---
+++ src/main/java/esg/GenerateEngineSound.java
@@ -0,0 +1,13 @@
+package esg;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
+
+public class GenerateEngineSound {
+
+	public static void main(String[] args) {
+        // TODO
+	}
+}
